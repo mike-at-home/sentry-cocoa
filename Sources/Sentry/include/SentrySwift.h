fad4f5d73e5c9fabe098ff58fc6e5adc7e5d8ce8
Mike Kasianowicz <mikekasiano@gmail.com>
2024-07-01 10:47:06 -0500

    Update SentrySwift.h

--- Sources/Sentry/include/SentrySwift.h
+++ Sources/Sentry/include/SentrySwift.h
@@ -7,10 +7,17 @@
 #    endif
 #endif
 
+#ifdef SENTRY_NO_UIKIT
+#if __has_include("SentryNoUIKit-Swift.h")
+#    import "SentryNoUIKit-Swift.h"
+#else
+#    import <SentryNoUIKit/SentryNoUIKit-Swift.h>
+#endif
+#else
 #if __has_include("Sentry-Swift.h")
 #    import "Sentry-Swift.h"
 #else
 #    import <Sentry/Sentry-Swift.h>
 #endif
-
+#endif // SENTRY_NO_UIKIT
 #endif
